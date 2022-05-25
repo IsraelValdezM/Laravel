0d8de2b00af75efd1e9b4c150fbd209f29889231
israelValdezM <israelvadezm1@gmail.com>
2022-05-25 10:06:01 -0700

    calcularedad

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ $router->get('/hi', function () use ($router) {
 });
 
 
-$router->get('/saludo', function(){
-    return 'hola mi nombe es israel';
+$router->get('/calcular/{edad}', function ($edad) {
+    if ($edad > 0 && $edad < 18) {
+        return 'Eres menor de edad';
+    } elseif($edad >= 18 && $edad <= 100){
+        return 'Eres mayor de edad';
+    } else{
+        return 'Edad erronea';
+    }
 });
 
 $router->post('/saludo', function(){
